3c6b69cd65076ae25817126ca737661bec77edcd
Yazdun <yazduntube@gmail.com>
2023-01-31 17:35:22 +0330

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { appRoutes } from './pages'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
 function App() {
-  return <div>react app</div>
+  return (
+    <div>
+      <Routes>
+        <Route>
+          {appRoutes.map(route => {
+            const { Component, path } = route
+            return <Route path={path} key={path} element={<Component />} />
+          })}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/AppRoutes.js
@@ -0,0 +1,9 @@
+import { Home } from './index'
+
+export const appRoutes = [
+  {
+    title: 'Homepage',
+    Component: Home,
+    path: '/',
+  },
+]
