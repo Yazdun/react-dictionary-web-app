6166a7252ed53fde87abe6b342e65a8140778e82
Yazdun <yazduntube@gmail.com>
2023-02-01 17:27:29 +0330

    commit

--- src/components/FontToggle.jsx
+++ src/components/FontToggle.jsx
@@ -1,10 +1,14 @@
 import { AnimatePresence } from 'framer-motion'
 import { useRef, useState } from 'react'
+import { useFont } from '../context'
 import { useOnClickOutside } from '../hooks'
 import { FontModal } from './FontModal'
+import { GoChevronDown } from 'react-icons/go'
+import cn from 'classnames'
 
 export const FontToggle = () => {
   const [modal, setModal] = useState(false)
+  const { font } = useFont()
   const ref = useRef(null)
 
   const toggle = () => setModal(prev => !prev)
@@ -12,7 +16,15 @@ export const FontToggle = () => {
 
   return (
     <div className="relative" ref={ref}>
-      <button onClick={toggle}>FontToggle</button>
+      <button className="flex items-center gap-2 capitalize" onClick={toggle}>
+        <span>{font.fontCategory}</span>
+        <GoChevronDown
+          className={cn(
+            'text-lg text-primary-100 transition-all duration-300',
+            modal && 'rotate-180 ',
+          )}
+        />
+      </button>
       <AnimatePresence initial={false} mode="wait">
         {modal && <FontModal />}
       </AnimatePresence>
