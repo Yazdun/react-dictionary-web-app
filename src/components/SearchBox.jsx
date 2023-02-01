d4dab1029fa5b1ba18565d4c55b2b9246d7ddfb2
Yazdun <yazduntube@gmail.com>
2023-02-02 00:28:34 +0330

    commit

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -16,21 +16,19 @@ export const SearchBox = () => {
   }
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 px-5 py-3 border-t dark:border-t-dark-100 md:static md:p-0 md:border-none">
-      <div className="relative">
-        <input
-          type="text"
-          className="w-full p-3 rounded-md md:p-5 bg-light-200 dark:bg-dark-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100"
-          placeholder="start typing ..."
-          onChange={inputChanged}
-        />
-        <div className="absolute top-0 bottom-0 right-0 p-3 rounded-md md:p-5 bg-light-200 dark:bg-dark-300">
-          {word && isLoading ? (
-            <Loading />
-          ) : (
-            <HiSearch className="mt-[0.1rem] text-xl text-primary-100" />
-          )}
-        </div>
+    <div className="relative">
+      <input
+        type="text"
+        className="w-full p-3 rounded-md md:p-5 bg-light-200 dark:bg-dark-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100"
+        placeholder="start typing ..."
+        onChange={inputChanged}
+      />
+      <div className="absolute top-0 bottom-0 right-0 p-3 rounded-md md:p-5 bg-light-200 dark:bg-dark-300">
+        {word && isLoading ? (
+          <Loading />
+        ) : (
+          <HiSearch className="mt-[0.1rem] text-xl text-primary-100" />
+        )}
       </div>
     </div>
   )
