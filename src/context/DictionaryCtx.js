6b3462252f9db6801ea5296a70a03eb3ecbbb098
Yazdun <yazduntube@gmail.com>
2023-02-02 22:33:42 +0330

    commit

--- src/context/DictionaryCtx.js
+++ src/context/DictionaryCtx.js
@@ -13,6 +13,15 @@ export function DictionaryProvider({ children }) {
     setWord(searchParams.get('word') || null)
   }, [])
 
+  useEffect(() => {
+    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth', // Optional if you want to skip the scrolling animation
+    })
+  }, [word])
+
   const { data, isLoading } = useModifiedSWR(word ? _getMeaning(word) : null)
 
   return (
--- src/framer.js
+++ src/framer.js
@@ -13,8 +13,8 @@ export const framer_font_check = {
 }
 
 export const framer_opacity = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0 },
+  initial: { opacity: 0, scale: 0.99 },
+  animate: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 0.99 },
   transition: { duration: 0.2 },
 }
