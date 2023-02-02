5e5db863ddc8c5d7bf1b14ad763a2d717581fcac
Yazdun <yazduntube@gmail.com>
2023-02-03 01:55:49 +0330

    commit

--- src/framer.js
+++ src/framer.js
@@ -21,8 +21,8 @@ export const framer_font_check = {
 }
 
 export const framer_opacity = {
-  initial: { opacity: 0, scale: 0.99 },
-  animate: { opacity: 1, scale: 1 },
-  exit: { opacity: 0, scale: 0.99 },
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
   transition: { duration: 0.2 },
 }
