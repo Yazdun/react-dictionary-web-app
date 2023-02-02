71683a2d46cc7203874a75e279ba9372bc65a939
Yazdun <yazduntube@gmail.com>
2023-02-02 18:08:33 +0330

    commit

--- src/components/SubSection.jsx
+++ src/components/SubSection.jsx
@@ -6,7 +6,7 @@ export const SubSection = ({ data }) => {
         <h2 className="relative pr-5 text-lg italic font-bold min-w-14 bg-light-100 dark:bg-dark-400">
           {partOfSpeech}
         </h2>
-        <div className="absolute top-3 left-0  -z-10 right-0 h-[0.1rem] rounded-full bg-light-200 dark:bg-dark-200"></div>
+        <div className="absolute top-4 left-0  -z-10 right-0 h-[0.1rem] rounded-full bg-light-200 dark:bg-dark-200"></div>
       </div>
 
       <div className="flex flex-col gap-3">
