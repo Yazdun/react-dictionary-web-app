2955e5e9f58f99d821d8e34c99a56a9cc7e40553
Yazdun <yazduntube@gmail.com>
2023-02-02 15:36:29 +0330

    commit

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import { HiSearch } from 'react-icons/hi'
 import { ImSpinner2 } from 'react-icons/im'
 import { useDictionary } from '../context'
+import { useScrollDirection } from '../hooks'
+import cn from 'classnames'
 
 export const SearchBox = () => {
   const [timer, setTimer] = useState(null)
   const { setWord, word, isLoading } = useDictionary()
+  const scrollDirection = useScrollDirection()
 
   const inputChanged = e => {
     clearTimeout(timer)
@@ -16,20 +19,28 @@ export const SearchBox = () => {
   }
 
   return (
-    <div className="sticky top-0 z-40 py-5 bg-light-100 dark:bg-dark-400">
-      <div className="relative px-5 custom_container">
-        <input
-          type="text"
-          className="w-full p-3 rounded-md md:p-5 bg-light-200 dark:bg-dark-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100"
-          placeholder="start typing ..."
-          onChange={inputChanged}
-        />
-        <div className="absolute top-0 bottom-0 right-0 p-3 rounded-md md:p-5 bg-light-200 dark:bg-dark-300">
-          {word && isLoading ? (
-            <Loading />
-          ) : (
-            <HiSearch className="mt-[0.1rem] text-xl text-primary-100" />
-          )}
+    <div
+      className={cn(
+        'sticky top-0 z-40 py-5 bg-light-100 dark:bg-dark-400',
+        scrollDirection > 90 &&
+          'border-b shadow-sm dark:shadow-none dark:border-b-dark-100',
+      )}
+    >
+      <div className="px-5 custom_container">
+        <div className="relative">
+          <input
+            type="text"
+            className="w-full p-3 rounded-md md:p-5 bg-light-200 dark:bg-dark-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100"
+            placeholder="start typing ..."
+            onChange={inputChanged}
+          />
+          <div className="absolute top-0 bottom-0 right-0 p-3 rounded-md md:p-5 bg-light-200 dark:bg-dark-300">
+            {word && isLoading ? (
+              <Loading />
+            ) : (
+              <HiSearch className="mt-[0.1rem] text-xl text-primary-100" />
+            )}
+          </div>
         </div>
       </div>
     </div>
